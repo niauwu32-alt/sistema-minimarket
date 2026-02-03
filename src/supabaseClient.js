4b0d9bec14e600e771dec644d4af30a20adbeaca
niauwu32-alt <niauwu32@gmail.com>
2026-02-03 00:50:28 -0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
+console.log('SUPABASE URL:', import.meta.env.VITE_SUPABASE_URL)
+console.log('SUPABASE KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY)
+
 export const supabase = createClient(
-  'https://iiuavhcrdjsfumqtlbvj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpdWF2aGNyZGpzZnVtcXRsYnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MzYxMDQsImV4cCI6MjA4NTQxMjEwNH0.EunS16EuC3DmTGEhG1xLQsSCPjsyJh40So-BgEDVHKs'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 )
