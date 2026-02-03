051d4f0a1deca9f31e566f43bbb081ac25dbc24c
Nia <niauwu32@gmail.com>
2026-02-03 12:06:55 -0500

    fix supabase createClient prod

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  console.error('❌ Variables de entorno de Supabase no definidas')
+}
+
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 )
